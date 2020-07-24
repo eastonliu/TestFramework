0473990ae8431373521ebda6fb479d965fee2359
liumingjun <405702878@qq.com>
2020-07-24 15:41:42 +0800

    优化变量命名

--- common/__init__.py
+++ common/__init__.py
@@ -6,66 +6,18 @@
 # @Desc   :
 
 import os
-import requests
-import base64
+from common.readYaml import YamlReader
+from common.getAccesstoken import get_accesstoken
 
 BASE_PATH = os.path.split(os.path.dirname(os.path.abspath(__file__)))[0]
+CONFIG_FILE = os.path.join(BASE_PATH, 'config', 'config.yaml')
 STATIC_PATH = os.path.join(BASE_PATH, 'static')
 DRIVER_PATH = os.path.join(BASE_PATH, 'drivers')
 API_PATH = os.path.join(BASE_PATH, 'api')
-LOGS_PATH = os.path.join(BASE_PATH,'logs')
-USERNAME = 'operator'
-PASSWORD = 'Test2020'
-URL = 'http://172.21.23.82'
-
-
-def get_accesstoken(url, username, password):
-    """
-    根据用户名和密码获取accesstoken
-    :param url:
-    :param username: 用户名
-    :param password: 密码
-    :return:
-    """
-    rec1 = requests.get(r'%s/zuul/api/checkIsFirst' % url, allow_redirects=False)
-    redirect_url1 = rec1.headers.get('Location')
-    # 获取第二个重定向地址,http://172.21.23.82/cas/?service=http%3A%2F%2F172.21.23.82%2Fzuul%2Fapi%2FcheckIsFirst
-    rec2 = requests.get(redirect_url1, allow_redirects=False)
-    redirect_url2 = rec2.headers.get('Location')
-    # 获取第三个重定向地址 http://172.21.23.82/cas/login?service=http%3A%2F%2F172.21.23.82%2Fzuul%2Fapi%2FcheckIsFirst
-    rec3 = requests.get(redirect_url2, allow_redirects=False)
-    redirect_url3 = rec3.headers.get('Location')
-    # 获取SESSION1
-    rec4 = requests.get(redirect_url3, allow_redirects=False)
-    SESSION = rec4.cookies.get('SESSION')
-    # 获取第四个重定向地址 http://172.21.23.82/zuul/api/checkIsFirst?ticket=ST-46-4Vw3O9p5g6aEIDSNfmecfje05nocms
-    _headers = {
-        'Cookie': 'SESSION=%s' % SESSION,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    payload = {
-        'username': username,
-        'password': str(base64.b64encode(password.encode()), 'ascii'),
-        'execution': 'e1s1',
-        '_eventId': 'submit',
-        'geolocation': ''
-    }
-    rec5 = requests.post(redirect_url3, allow_redirects=False, headers=_headers, data=payload)
-    redirect_url4 = rec5.headers.get('Location')
-    # 获取第五个重定向地址 http://172.21.23.82/zuul/api/checkIsFirst;jsessionid=EBBE8F4E39FBD55A564C4097ED53683D
-    rec6 = requests.get(redirect_url4, allow_redirects=False)
-    JSESSIONID = rec6.cookies.get('JSESSIONID')
-    redirect_url5 = rec6.headers.get('Location')
-    # 获取accessToken
-    _headers = {
-        'Cookie': 'JSESSIONID=%s' % JSESSIONID,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    rec7 = requests.get(redirect_url5, headers=_headers)
-    accessToken = rec7.cookies.get('accessToken')
-    return accessToken
-
-
+LOGS_PATH = os.path.join(BASE_PATH, 'logs')
+USERNAME = YamlReader(CONFIG_FILE).data['username']
+PASSWORD = YamlReader(CONFIG_FILE).data['password']
+URL = YamlReader(CONFIG_FILE).data['host']
 ACCESSTOKEN = get_accesstoken(url=URL, username=USERNAME, password=PASSWORD)
 HEADERS = {
     'accessToken': ACCESSTOKEN
